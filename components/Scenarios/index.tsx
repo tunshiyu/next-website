38f2f174af7ce384c7870778ab609f8d97dfbbf5
tunshiyu <55213426@qq.com>
2019-08-23 17:48:06 +0800

    map优化

--- components/Scenarios/index.tsx
+++ components/Scenarios/index.tsx
@@ -4,7 +4,7 @@
  * @作者: 陈杰
  * @Date: 2019-07-30 11:59:39
  * @LastEditors: 于效仟
- * @LastEditTime: 2019-08-23 17:23:12
+ * @LastEditTime: 2019-08-23 17:47:51
  */
 import React, { useState } from 'react';
 import styles from './index.module.less';
@@ -13,6 +13,8 @@ export default function Scenarios() {
   const [flag, handleFlag] = useState(true);
   // 点击按钮滑动距离
   const slideDistance = '525px';
+  // 场景
+  const scenarios = ['物流', '工业', '供应链', '酒店', '智慧城市', '机场', '光伏系能源'];
   function slide() {
     if (flag) {
       document.getElementById('slideList')!.style.transform = `translateX(-${slideDistance})`;
@@ -26,13 +28,9 @@ export default function Scenarios() {
       <div className={styles.header}>使用场景</div>
       <div className={styles.wrap}>
         <ul className={styles.list} id="slideList">
-          <li>物流</li>
-          <li>工业</li>
-          <li>供应链</li>
-          <li>酒店</li>
-          <li>智慧城市</li>
-          <li>机场</li>
-          <li>光伏系能源</li>
+          {scenarios.map((scenario, index) => (
+            <li key={index}>{scenario}</li>
+          ))}
         </ul>
         <div className={flag ? styles.btnLeft : styles.btnRight} onClick={slide}></div>
       </div>
