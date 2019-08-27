dc03ed17716dcf677f28dbf99925d709ef227aa7
tunshiyu <55213426@qq.com>
2019-08-27 09:25:12 +0800

    解决打包冲突

--- pages/about/components/Advantage/index.tsx
+++ pages/about/components/Advantage/index.tsx
@@ -5,13 +5,14 @@ import { AdvantageItem } from 'interfaces';
 export default function Advantage({ advantages }: { advantages: AdvantageItem[] }) {
   return (
     <div className={styles.container}>
-      {advantages.map((item, index) => (
-        <div className={styles.advantage} key={index}>
-          <img alt="团队优势" src={item.img} className={styles.img} />
-          <div className={styles.title}>{item.title}</div>
-          <div className={styles.subtitle}>{item.subtitle}</div>
-        </div>
-      ))}
+      {advantages &&
+        advantages.map((item, index) => (
+          <div className={styles.advantage} key={index}>
+            <img alt="团队优势" src={item.img} className={styles.img} />
+            <div className={styles.title}>{item.title}</div>
+            <div className={styles.subtitle}>{item.subtitle}</div>
+          </div>
+        ))}
     </div>
   );
 }
