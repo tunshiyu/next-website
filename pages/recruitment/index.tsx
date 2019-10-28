587455e4f51f9f70a112be4666ba062863d827dd
yuxiaoqian <552134826@qq.com>
2019-10-28 10:23:13 +0800

    refactor: 加入我们模块
    Differential Revision: http://cr.thundersdata.com:26170/D5775

--- pages/recruitment/index.tsx
+++ pages/recruitment/index.tsx
@@ -4,9 +4,9 @@
  * @作者: 于效仟
  * @Date: 2019-08-20 19:05:23
  * @LastEditors: 于效仟
- * @LastEditTime: 2019-09-03 11:25:53
+ * @LastEditTime: 2019-10-28 10:16:48
  */
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './index.module.less';
 import Head from 'next/head';
 import Layout from '@components/Layout';
@@ -14,40 +14,15 @@ import Header from '@components/Header';
 import ContactUs from '@components/ContactUs';
 import Footer from '@components/Footer';
 import Banner from '@components/Banner';
-import Drawer from '@components/Drawer';
-import { TECHNICALS, PRODUCTS, DESIGNS, OPERATINGS } from '../../constant';
 
 export default function Recruitment() {
   const banner = {
     img: '/static/recruitment/banner@2x.png',
-    text: '人才招聘',
+    text: '加入我们',
+    subtext:
+      '我们是一个充满活力的团队，热爱大数据，并在这条路上专注耕耘多年，未来我们期待和你一起砥砺前行',
   };
-  // 全部职位
-  const all = TECHNICALS.concat(PRODUCTS, DESIGNS, OPERATINGS);
-  const btnNodes = [
-    {
-      text: '全部',
-      node: all,
-    },
-    {
-      text: '技术类',
-      node: TECHNICALS,
-    },
-    {
-      text: '产品类',
-      node: PRODUCTS,
-    },
-    {
-      text: '设计类',
-      node: DESIGNS,
-    },
-    {
-      text: '运营类',
-      node: OPERATINGS,
-    },
-  ];
-  const [activeKey, handleActiveKey] = useState(0);
-  const [data, handleData] = useState(all);
+
   return (
     <>
       <Head>
@@ -65,30 +40,27 @@ export default function Recruitment() {
           <Header selectedKeys={['6-2']} />
           <Banner banner={banner} />
           <div className={styles.wrap}>
-            {/* 职位类型  */}
-            <div className={styles.type}>
-              <span className={styles.text}>职位类型:</span>
-              <div className={styles.btns}>
-                {btnNodes.map((item, index) => {
-                  return (
-                    <a
-                      key={index}
-                      className={activeKey === index ? styles.active : ''}
-                      onClick={() => {
-                        handleActiveKey(index);
-                        handleData(item.node);
-                      }}
-                    >
-                      {item.text}
-                    </a>
-                  );
-                })}
+            <div className={styles.joinCard}>
+              <span className={styles.title}>社会招聘</span>
+              <span className={styles.text}>
+                我们为你提供绽放才华的舞台，和我们并肩作战，一起用大数据赋能企业，服务客户。
+              </span>
+              <div
+                className={styles.btn}
+                onClick={() =>
+                  window.open(
+                    'https://www.zhipin.com/gongsir/592f45f10d17ded01nZ93Ni9Ew~~.html?ka=company-jobs',
+                    '_blank',
+                  )
+                }
+              >
+                申请社招岗位
               </div>
             </div>
-            {/* 招聘岗位-抽屉模块 */}
-            {data.map((item, index) => (
-              <Drawer key={index} {...item} />
-            ))}
+            <span className={styles.subText}>
+              * 或在邮件中写明投递岗位，将简历直接发送至
+              <span className={styles.emailTetx}>hr@thundersdata.com</span>
+            </span>
           </div>
           <ContactUs />
           <Footer />
